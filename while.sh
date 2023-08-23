e3550081bb875116760dcf9d1820b7c1cd1d51d2
Ashish Gawali <64855416+ashish200214@users.noreply.github.com>
2023-08-24 00:59:03 +0530

    Update while.sh

--- while.sh
+++ while.sh
@@ -3,3 +3,14 @@ while [[ $i -ve 100 ]]; do
     echo "$i"
     (( i += 1 ))
 done
+
+
+
+#for loop
+echo "Enter value tou want to print from 1 to n"
+read n
+
+
+for (( i=1; i<=n; i++)); do
+  echo "$i" 
+  done
